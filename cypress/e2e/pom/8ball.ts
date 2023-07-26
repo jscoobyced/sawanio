2e18eebae2ef1a4efd49752d800e6e93b11034d3
Cédric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2023-07-26 20:36:18 +0700

    Add timeout for tests

--- cypress/e2e/pom/8ball.ts
+++ cypress/e2e/pom/8ball.ts
@@ -1,11 +1,13 @@
 const check8Ball = () => {
   cy.get('img[data-id="8ball-image"]').should('be.visible')
   cy.get('span[data-id="fortune-answer"]').should('not.contain.text')
-  cy.get('img[data-id="8ball-image"]').click().then(() => {
-    cy.wait(3000).then(() => {
-      cy.get('span[data-id="fortune-answer"]').invoke('text').should('not.be.empty')
+  cy.get('img[data-id="8ball-image"]')
+    .click()
+    .then(() => {
+      cy.get('span[data-id="fortune-answer"]', { timeout: 12000 })
+        .invoke('text')
+        .should('not.be.empty')
     })
-  })
 }
 
 export default check8Ball
