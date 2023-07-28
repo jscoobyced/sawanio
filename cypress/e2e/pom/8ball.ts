ce597bcefb04975126b9ab79abffd42155f1126b
Cédric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2023-07-28 11:11:20 +0700

    Move timeout in the tests.

--- cypress/e2e/pom/8ball.ts
+++ cypress/e2e/pom/8ball.ts
@@ -2,11 +2,11 @@ const check8Ball = () => {
   cy.get('img[data-id="8ball-image"]').should('be.visible')
   cy.get('span[data-id="fortune-answer"]').should('not.contain.text')
   cy.get('img[data-id="8ball-image"]')
-    .click({ timeout: 12000 })
+    .click()
     .then(() => {
       cy.get('span[data-id="fortune-answer"]')
         .invoke('text')
-        .should('not.be.empty')
+        .should('not.be.empty',{ timeout: 12000 })
     })
 }
 
